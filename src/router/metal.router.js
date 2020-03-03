817e4df41e57417e2caeec3ea758ce3bc1034e4b
Sameer Al Harbi <smrhrbi@gmail.com>
2020-03-03 19:59:21 +0300

    random results

--- src/router/metal.router.js
+++ src/router/metal.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const router = new express.Router();
 
 const upload = multer({
@@ -14,11 +15,33 @@ const upload = multer({
     }
 });
 
-router.post('/inspect', upload.single('inspect'),(req, res) => {
-    res.send({
+const results = [
+    {
         gold: 67,
         iron: 26
-    });
+    }, {
+        silver: 44,
+        sand: 22
+    }, {
+        volcano: 80,
+        iron: 26
+    }, {
+        copper: 48,
+        gold: 12
+    }, {
+        basalt: 33,
+        sand: 11
+    }, {
+        quartz: 17,
+        rock: 11
+    }
+]
+
+router.post('/inspect', upload.single('inspect'),(req, res) => {
+    const buffer = sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    console.log(req.body);
+    console.log(req.body.lat);
+    res.send(results[Math.floor(Math.random() * Math.floor(5))]);
 }, (error, req, res, next) => {
     res.status(400).send({error: error.message});
 });
